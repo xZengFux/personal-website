0ebf40215de5ca833f647c00f6cdc50adca1eea8
xZengFux <z8fu@ucsd.edu>
2020-06-04 18:22:50 -0700

    update resume

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,6 +1,6 @@
 module.exports = [{
       plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
-      options: {"plugins":[],"name":"BrittanyChiang","short_name":"BrittanyChiang","start_url":"/","background_color":"#020c1b","theme_color":"#0a192f","display":"minimal-ui","icon":"src/images/logo.png"},
+      options: {"plugins":[],"name":"ZengFu","short_name":"ZengFu","start_url":"/","background_color":"#020c1b","theme_color":"#0a192f","display":"minimal-ui","icon":"src/images/logo.png"},
     },{
       plugin: require('../node_modules/gatsby-plugin-offline/gatsby-browser.js'),
       options: {"plugins":[]},
--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -9,7 +9,7 @@ var plugins = [{
       options: {"plugins":[]},
     },{
       plugin: require('/Users/zengfu/Documents/GitHub/personal-website/personal-website/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
-      options: {"plugins":[],"name":"BrittanyChiang","short_name":"BrittanyChiang","start_url":"/","background_color":"#020c1b","theme_color":"#0a192f","display":"minimal-ui","icon":"src/images/logo.png"},
+      options: {"plugins":[],"name":"ZengFu","short_name":"ZengFu","start_url":"/","background_color":"#020c1b","theme_color":"#0a192f","display":"minimal-ui","icon":"src/images/logo.png"},
     },{
       plugin: require('/Users/zengfu/Documents/GitHub/personal-website/personal-website/node_modules/gatsby-plugin-offline/gatsby-ssr'),
       options: {"plugins":[]},
